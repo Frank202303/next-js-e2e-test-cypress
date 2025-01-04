2b4820402c94cc682daec55a51d2fa35842aef96
frankzhang <frank.zhang.sa.au@gmail.com>
2025-01-04 15:46:36 +1000

    1. npx cypress open 2. use Regular Expressions to check URL

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -6,4 +6,13 @@ describe('register page', () => {
     cy.get('[data-cy="emailError"]').should('exist');
     cy.get('[data-cy="passwordError"]').should('exist');
   })
+
+  it('Should redirect to success page when filling the form and click', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-cy="emailInput"]').type("name1.l@qq.com")
+    cy.get('[data-cy="passwordInput"]').type("name1pwd")
+
+    cy.get('[data-cy="submit"]').click();
+    cy.url().should('match', /\/success$/)
+  })
 })
